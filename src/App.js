1039d68198df45f9903b031a667fa1004f8a9f3c
TalelJandoubi <talel.axis@gmail.com>
2024-07-24 15:29:43 +0200

    Add vercel.json for proper routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // src/App.js
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
